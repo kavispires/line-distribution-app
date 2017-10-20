576a40d52c200c06f4dab45afc8d9dab80b5a43f
kavispires <kavispires@gmail.com>
2017-10-20 15:43:30 -0700

    Basic Files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+import store from './store';
+
+import AppContainer from './containers/AppContainer';
+
+import './stylesheets/index.css';
+
+ReactDOM.render(
+	<Provider store={store}>
+		<AppContainer />
+	</Provider>, document.getElementById('root'));
+registerServiceWorker();
