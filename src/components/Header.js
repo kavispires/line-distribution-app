73633df4a78da3d4e3166489d38c86ecb436b3de
kavispires <kavispires@gmail.com>
2017-11-29 00:07:04 -0800

    Fixed logo in deployed version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.svg';
 
 const Header = () => {
   return (
     <header className="ld-header">
-      <img className="ld-logo" src="/logo.svg" alt="Logo" />
+      <img className="ld-logo" src={logo} alt="Logo" />
       <nav className="ld-nav">
-      	<Link to="/artists">Artists</Link>
-      	<Link to="/distribute">Distribute</Link>
-      	<Link to="/create">Create</Link>
-      	<Link to="/lyrics">Lyrics</Link>
-      	<Link to="/colorsheet">Color Sheet</Link>
+        <Link to="/artists">Artists</Link>
+        <Link to="/distribute">Distribute</Link>
+        <Link to="/create">Create</Link>
+        <Link to="/lyrics">Lyrics</Link>
+        <Link to="/colorsheet">Color Sheet</Link>
       </nav>
     </header>
   );
