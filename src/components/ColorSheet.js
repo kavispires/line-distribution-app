0aa1ecc2d8a5c82568782437c68ae8f57b0105e1
kavispires <kavispires@gmail.com>
2017-12-23 10:57:04 -0800

    Convert to double spaces only

--- src/components/ColorSheet.js
+++ src/components/ColorSheet.js
@@ -5,20 +5,20 @@ import { COLORS_LIST } from '../constants';
 const ColorSheet = (props) => {
   return (
     <div className="container">
-        <h1>Color Sheet</h1>
-        <small>The numbers indicate how many times each color appears in the database.</small>
-        <ul className="color-palette">
-        {
-            COLORS_LIST.map(color => (
-                <li key={`${color}`} className={`palette color-${color}`}>
-                    {props.app.colorCount[color]}<br />
-            {color}
-                </li>
-            ))
-        }
-        </ul>
+      <h1>Color Sheet</h1>
+      <small>The numbers indicate how many times each color appears in the database.</small>
+      <ul className="color-palette">
+      {
+        COLORS_LIST.map(color => (
+          <li key={`${color}`} className={`palette color-${color}`}>
+          {props.app.colorCount[color]}<br />
+          {color}
+          </li>
+          ))
+      }
+      </ul>
     </div>
-  );
+    );
 };
 
 export default ColorSheet;
