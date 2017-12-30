e52c904891864b7870600a0b2510c85e365901b4
kavispires <kavispires@gmail.com>
2017-12-29 18:13:23 -0800

    Removed useless constructor

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor (props) {
-    super(props);
-
-  }
-
   componentWillMount () {
     this.props.parseArtists();
   }
