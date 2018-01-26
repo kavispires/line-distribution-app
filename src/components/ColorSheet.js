ee7c32160e7f026dc914cc3cff18dbb46f3f0b29
Fabiano Pires <kavispires@Fabianos-MacBook-Pro.local>
2018-01-26 14:05:42 -0800

    Updated Color Sheet

--- src/components/ColorSheet.js
+++ src/components/ColorSheet.js
@@ -6,20 +6,20 @@ const ColorSheet = (props) => {
       <h1>Color Sheet</h1>
       <small>The numbers indicate how many times each color appears in the database.</small>
       <ul className="color-palette">
-      {
-        Object.keys(props.app.colorList).map(id => {
+        {
+        Object.keys(props.app.colorList).map((id) => {
           const color = props.app.colorList[id].name;
           return (
             <li key={color} className={`palette color-${color}`}>
-            {props.app.colorCount[color]}<br />
-            {color}
+              {props.app.colorCount[color]}<br />
+              {color}
             </li>
-          )
+          );
         })
       }
       </ul>
     </div>
-    );
+  );
 };
 
 export default ColorSheet;
