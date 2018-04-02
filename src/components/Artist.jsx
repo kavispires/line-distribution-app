3cc62bad07e856042f889767f61f8fa7ae84da49
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2018-04-02 00:10:39 -0700

    Moved Songs Table to its own component

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import _ from 'lodash';
 
 import Member from './Member';
 import iconOfficial from '../images/icon-official.svg';
-import iconYes from '../images/icon-yes.svg';
-import iconNo from '../images/icon-no.svg';
+import ArtistSongsTable from './ArtistSongsTable';
 
 class Artist extends Component {
   componentDidMount() {
@@ -44,8 +42,6 @@ class Artist extends Component {
 
       // props.history.push(`/distribute`);
     };
-
-    const sortedSelectedUnitSongs = _.sortBy(selectedUnitSongs, ['title']);
 
     return (
       <section className="container">
@@ -103,70 +99,11 @@ class Artist extends Component {
               </div>
               <h3>Songs:</h3>
               {
-                selectedUnitSongs && selectedUnitSongs.length > 0 ? (
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th>Title</th>
-                        <th>Type</th>
-                        <th>Lyrics</th>
-                        <th>Distribution</th>
-                      </tr>
-                    </thead>
-                    <tbody onClick={(e) => handleSongClick(e)}>
-                      {
-                        sortedSelectedUnitSongs && sortedSelectedUnitSongs.map((song) => {
-                          let type = 'Official';
-                          if (song.type === 'would') {
-                            type = `Originally by ${song.originalArtist}`;
-                          } else if (song.type === 'should') {
-                            type = "How it should've been";
-                          }
-                          // const songDistribution = this.props.parseSong(song);
-                          return (
-                            <tr key={song.id}>
-                              <td>{song.title}</td>
-                              <td>{type}</td>
-                              <td>
-                                {
-                                  song.lyrics ?
-                                    <img className="icon icon-tab" src={iconYes} alt="Yes" />
-                                    :
-                                    <img className="icon icon-tab" src={iconNo} alt="No" />
-                                }
-                              </td>
-                              <td>
-                                {
-                                  song.result ?
-                                    (
-                                      <span className="unit-songs-dist">
-                                        {
-                                          song.result.map((instance) => {
-                                            const { colorId } = database.members[instance.memberId];
-                                            const barWidth = instance.memberTotal;
-                                            return (
-                                              <span
-                                                key={`${song.id}-${colorId}-${instance.memberId}`}
-                                                className={`unit-songs-member color-${colorId} bar-width-${barWidth}`}
-                                              />
-                                            );
-                                          })
-                                        }
-                                      </span>
-                                    )
-                                    :
-                                      <img className="icon icon-tab" src={iconNo} alt="No" />
-                                }
-                              </td>
-                            </tr>
-                          );
-                        })
-                      }
-                    </tbody>
-                  </table>
-                ) : (
-                  <p>No songs available</p>
-                )
+                <ArtistSongsTable
+                  database={database}
+                  selectedUnitSongs={selectedUnitSongs}
+                  handleSongClick={handleSongClick}
+                />
               }
 
             </section>
