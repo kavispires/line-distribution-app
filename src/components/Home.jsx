99f7f9819b42dd21e109d43f19fa263390d5b9e9
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2018-10-13 21:32:49 -0700

    Fixed Icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import logo from '../images/logo-alt.svg';
+
 import { YOUTUBE_URLS } from '../constants';
+
+import Icon from './Icon';
 
 const Home = props => {
   const youtubeUrl =
     YOUTUBE_URLS[Math.floor(Math.random() * YOUTUBE_URLS.length)];
+
+  const handleLearnMoreClick = () => {
+    props.history.push('/learn-more');
+  };
+
   return (
     <main className="container container--no-padding">
       <div className="home__video">
@@ -22,9 +30,13 @@ const Home = props => {
         <img className="home__logo" src={logo} alt="Line Distribution" />
         <div className="home__buttons">
           {/* {!props.auth.isAuthenticated ? ( */}
-          <button className="btn-home">Sign-in</button>
+          <button className="btn-home">
+            Sign-in <Icon type="logout" color="white" inline />
+          </button>
           {/* ) : null} */}
-          <button className="btn-home">Learm more</button>
+          <button className="btn-home" onClick={() => handleLearnMoreClick()}>
+            Learm more
+          </button>
         </div>
       </div>
     </main>
@@ -34,6 +46,7 @@ const Home = props => {
 Home.propTypes = {
   // auth: PropTypes.object.isRequired,
   // login: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
 export default Home;
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { ICONS } from '../constants';
+import { ICONS, ICON_COLORS } from '../constants';
 
-const Icon = ({ type, size, color }) => {
+const Icon = ({ type, size, color, inline }) => {
+  const margin = inline ? '0 3px' : 0;
+
   const styles = {
     svg: {
       display: 'inline-block',
       verticalAlign: 'middle',
+      margin,
     },
     path: {
-      fill: color,
+      fill: ICON_COLORS[color],
     },
   };
-
-  console.log('Icon props', type, color, size);
 
   return (
     <svg
       style={styles.svg}
       width={`${size}px`}
       height={`${size}px`}
-      viewBox="0 0 1024 1024"
+      viewBox="0 0 100 100"
     >
       <path style={styles.path} d={ICONS[type]} />
     </svg>
@@ -32,11 +33,13 @@ Icon.propTypes = {
   type: PropTypes.string.isRequired,
   size: PropTypes.number,
   color: PropTypes.string,
+  inline: PropTypes.bool,
 };
 
 Icon.defaultProps = {
-  size: 16,
-  color: '#ff0000',
+  size: 12,
+  color: 'default',
+  inline: false,
 };
 
 export default Icon;
