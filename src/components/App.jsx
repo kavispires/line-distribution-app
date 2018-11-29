bcb5aae958421b7cf36252e8cf59d3a2330667e9
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2018-11-28 22:47:24 -0800

    Wrote API get calls

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,18 @@ class App extends Component {
     try {
       console.log('==== TRY/CATCH START ====');
       const startDB = await API.init();
+      API.setter(true);
       console.log(startDB);
-      const get = API.get();
+      const test = await API.get(`/users/hbFlRswbZkepQfaONzoyB6EuJSA2`);
+      // console.log(test);
+      const test2 = await API.get(`/artists/-LDcyPgcJaR4gd8pvVKb/units`);
+      const test3 = await API.get(`/units/-LDcyPgbytNzK2BkYaLN`);
+      const test4 = await API.get('/colors');
+      const test5 = await API.get(`/members/-LDcyPg_LQgDOBQ-wFP3`);
+      const test6 = await API.get(`/songs/-LLcdNSHFJj1TqkK0y9f`);
+      const test7 = await API.get(`/songs`);
+      const test8 = await API.get(`units/-LDcyPgbytNzK2BkYaLM/members`);
+      console.log(test8);
     } catch (error) {
       console.log('ERROR BITCH!!!', error);
     } finally {
