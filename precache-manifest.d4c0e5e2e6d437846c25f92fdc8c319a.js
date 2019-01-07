145547aba41f1de11f2243469d21b43d2834ec89
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-01-06 16:50:53 -0800

    Updates

---
+++ precache-manifest.d4c0e5e2e6d437846c25f92fdc8c319a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "94d4fc32bfc9ddd6aabc1d4cef245fe2",
+    "url": "/line-distribution-app/static/media/user-placeholder.94d4fc32.svg"
+  },
+  {
+    "revision": "d7f959902ce50797f31ae5ec55d0f72b",
+    "url": "/line-distribution-app/static/media/logo.d7f95990.svg"
+  },
+  {
+    "revision": "048816e58c5e78ee9198639ff44c9ee8",
+    "url": "/line-distribution-app/static/media/logo-neg.048816e5.svg"
+  },
+  {
+    "revision": "6de76e88902ff9534abd8c551ce1b3b3",
+    "url": "/line-distribution-app/static/media/logo-alt.6de76e88.svg"
+  },
+  {
+    "revision": "e2f055bc3ba717da6bfc",
+    "url": "/line-distribution-app/static/js/runtime~main.e2f055bc.js"
+  },
+  {
+    "revision": "6e556fbc8e39c6a7eb57",
+    "url": "/line-distribution-app/static/js/main.6e556fbc.chunk.js"
+  },
+  {
+    "revision": "a841044c8f27d3aa9b04",
+    "url": "/line-distribution-app/static/js/1.a841044c.chunk.js"
+  },
+  {
+    "revision": "6e556fbc8e39c6a7eb57",
+    "url": "/line-distribution-app/static/css/main.8d717cf3.chunk.css"
+  },
+  {
+    "revision": "a288a65dd374ae7ccbaee3ee7b71e8bd",
+    "url": "/line-distribution-app/index.html"
+  }
+];
