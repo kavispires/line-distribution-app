f1877c2a510e06542273c4d86e85dca9dda83e66
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-02-05 21:18:03 -0800

    Removed console.log from LoadingIcon component

--- src/app/common/components/LoadingIcon.jsx
+++ src/app/common/components/LoadingIcon.jsx
@@ -4,14 +4,11 @@ import PropTypes from 'prop-types';
 // Import images
 import loading from '../../../images/loading.svg';
 
-const LoadingIcon = ({ size }) => {
-  console.log('SIZE', size);
-  return (
-    <span className={`loading-icon loading-icon--${size}`}>
-      <img className="loading-icon__image" src={loading} alt="Loading Icon" />
-    </span>
-  );
-};
+const LoadingIcon = ({ size }) => (
+  <span className={`loading-icon loading-icon--${size}`}>
+    <img className="loading-icon__image" src={loading} alt="Loading Icon" />
+  </span>
+);
 
 LoadingIcon.propTypes = {
   size: PropTypes.string,
