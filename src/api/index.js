f262566de117d03b3319877b006ba330a12d5bbd
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-02-05 21:26:14 -0800

    Fixed api cache bug

--- src/api/index.js
+++ src/api/index.js
@@ -23,6 +23,16 @@ export const db = {
   users: {},
 };
 
+export const fullyLoaded = {
+  artists: false,
+  colors: false,
+  distributions: false,
+  members: false,
+  positions: false,
+  songs: false,
+  units: false,
+};
+
 export let dbRef = null; // eslint-disable-line
 
 class API {
@@ -552,13 +562,14 @@ class API {
 const getFunctions = {
   // Fetches all artists
   fetchArtists: async () => {
-    if (Object.keys(db.artists).length < 2) {
+    if (fullyLoaded.artists === false) {
       // TO-DO: Add 00 id to database, remove it in the serializer collection for a better cache check
       let response = {};
       await dbRef.ref(`/artists`).once('value', snapshot => {
         response = snapshot.val();
       });
       db.artists = response;
+      fullyLoaded.artists = true;
     }
     return serializeCollection(db.artists, 'artist');
   },
@@ -583,12 +594,13 @@ const getFunctions = {
   },
   // Fetches all colors
   fetchColors: async () => {
-    if (Object.keys(db.colors).length === 0) {
+    if (fullyLoaded.colors === false) {
       let response = {};
       await dbRef.ref(`/colors`).once('value', snapshot => {
         response = snapshot.val();
       });
       db.colors = response;
+      fullyLoaded.artists = true;
     }
     return serializeCollection(db.colors, 'color');
   },
@@ -616,12 +628,13 @@ const getFunctions = {
   },
   // Fetches all members
   fetchMembers: async () => {
-    if (Object.keys(db.members).length === 0) {
+    if (fullyLoaded.members === false) {
       let response = {};
       await dbRef.ref(`/members`).once('value', snapshot => {
         response = snapshot.val();
       });
       db.members = response;
+      fullyLoaded.members = true;
     }
     return serializeCollection(db.members, 'member');
   },
@@ -655,13 +668,14 @@ const getFunctions = {
   },
   // Fetches all songs
   fetchSongs: async () => {
-    if (Object.keys(db.songs).length === 0) {
+    if (fullyLoaded.songs === false) {
       // TO-DO: Add 00 id to database, remove it in the serializer collection for a better cache check
       let response = {};
       await dbRef.ref(`/songs`).once('value', snapshot => {
         response = snapshot.val();
       });
       db.songs = response;
+      fullyLoaded.songs = true;
     }
     return serializeCollection(db.songs, 'song');
   },
@@ -739,6 +753,7 @@ const postFunctions = {
       }
     });
     db.artists[key] = response;
+    fullyLoaded.artists = false;
     return serialize.artist(db.artists[key]);
   },
   // Creates single color
@@ -756,6 +771,7 @@ const postFunctions = {
       }
     });
     db.colors[key] = response;
+    fullyLoaded.colors = false;
     return serialize.color(db.colors[key]);
   },
   // Creates single distribution
@@ -774,6 +790,7 @@ const postFunctions = {
       }
     });
     db.members[key] = response;
+    fullyLoaded.members = false;
     return serialize.member(db.members[key]);
   },
   // Creates single song
@@ -790,6 +807,7 @@ const postFunctions = {
       }
     });
     db.songs[key] = response;
+    fullyLoaded.songs = false;
     return serialize.song(db.songs[key]);
   },
   // Creates single unit
@@ -806,6 +824,7 @@ const postFunctions = {
       }
     });
     db.units[key] = response;
+    fullyLoaded.units = false;
     return serialize.unit(db.units[key]);
   },
   // Creates single user
@@ -842,6 +861,7 @@ const putFunctions = {
       response = snapshot.val();
     });
     db.artists[key] = response;
+    fullyLoaded.artists = false;
     return serialize.artist(db.artists[key]);
   },
   // Updates single distribution
@@ -862,6 +882,7 @@ const putFunctions = {
       response = snapshot.val();
     });
     db.members[key] = response;
+    fullyLoaded.members = false;
     return serialize.member(db.members[key]);
   },
   // Updates single song
@@ -879,6 +900,7 @@ const putFunctions = {
       response = snapshot.val();
     });
     db.songs[key] = response;
+    fullyLoaded.songs = false;
     return serialize.song(db.songs[key]);
   },
   // Updates single unit
@@ -896,6 +918,7 @@ const putFunctions = {
       response = snapshot.val();
     });
     db.units[key] = response;
+    fullyLoaded.units = false;
     return serialize.unit(db.units[key]);
   },
   // Updates single user
