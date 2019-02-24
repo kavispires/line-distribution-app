b8fad62d5608b581117af44ffd2523248e6aa791
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-02-23 22:56:37 -0800

    Added scroll sync to Lyrics page input/output

--- src/app/pages/lyrics/components/Lyrics.jsx
+++ src/app/pages/lyrics/components/Lyrics.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync';
 
 // Import lyrics components
 import LyricsOutput from './LyricsOutput';
@@ -7,90 +8,6 @@ import LyricsOutput from './LyricsOutput';
 import { Loading, RequirementWrapper } from '../../../common';
 // Import lyric parser
 import parseLyrics from '../parser';
-
-const MEMBERS = {
-  '-LVRFeNAc2nf-uZlIurE': {
-    colorId: 'col000020',
-    color: {
-      b: 255,
-      count: 8,
-      g: 111,
-      hex: '#406FFF',
-      name: 'blue',
-      number: 20,
-      r: 64,
-      id: 'col000020',
-    },
-    name: 'Kyle',
-    id: '-LVRFeNAc2nf-uZlIurE',
-  },
-  '-LVRFeOG1HJSZSdHbotB': {
-    colorId: 'col000002',
-    color: {
-      b: 27,
-      count: 5,
-      g: 49,
-      hex: '#CE311B',
-      name: 'blood',
-      number: 2,
-      r: 206,
-      id: 'col000002',
-    },
-    name: 'Anthony',
-    modifiedBy: 'hbFlRswbZkepQfaONzoyB6EuJSA2',
-    nationality: 'OTHER',
-    positions: ['VISUAL', 'VOCALIST'],
-    private: true,
-    referenceArtist: 'KVS',
-    id: '-LVRFeOG1HJSZSdHbotB',
-  },
-  '-LVRFePJxP4fCWgN_Lqf': {
-    colorId: 'col000008',
-    color: {
-      b: 0,
-      count: 8,
-      g: 228,
-      hex: '#FFE400',
-      name: 'yellow',
-      number: 8,
-      r: 255,
-      id: 'col000008',
-    },
-    name: 'Viktor',
-    id: '-LVRFePJxP4fCWgN_Lqf',
-  },
-  '-LVRFeQMJ5rcbg-U-KRY': {
-    colorId: 'col000023',
-    color: {
-      b: 239,
-      count: 9,
-      g: 119,
-      hex: '#A177EF',
-      name: 'violet',
-      number: 23,
-      r: 161,
-      id: 'col000023',
-    },
-    name: 'Ira',
-    id: '-LVRFeQMJ5rcbg-U-KRY',
-  },
-  '-LVRFeRPTdA6WiOv1CO7': {
-    colorId: 'col000012',
-    color: {
-      b: 23,
-      count: 9,
-      g: 184,
-      hex: '#7FB817',
-      name: 'green',
-      number: 12,
-      r: 127,
-      id: 'col000012',
-    },
-    name: 'Seth',
-    modifiedBy: 'hbFlRswbZkepQfaONzoyB6EuJSA2',
-    id: '-LVRFeRPTdA6WiOv1CO7',
-  },
-};
 
 class Lyrics extends Component {
   constructor(props) {
@@ -119,9 +36,8 @@ class Lyrics extends Component {
 
     const handleLyricsInput = event => {
       const { value } = event.target;
-
       this.setState({
-        lyrics: parseLyrics(value, activeUnit.members, activeUnit.id),
+        lyrics: parseLyrics(value, activeUnit.members || [], activeUnit.id),
       });
     };
 
@@ -129,17 +45,26 @@ class Lyrics extends Component {
       <RequirementWrapper requirements={['activeUnit']}>
         <main className="container container--lyrics">
           <h1>Lyrics</h1>
-          <section className="lyrics__group">
-            <textarea
-              name="lyrics"
-              id=""
-              cols="30"
-              rows="10"
-              className="lyrics__input"
-              onChange={handleLyricsInput}
-            />
-            <LyricsOutput lyrics={this.state.lyrics.result} colorsDB={colors} />
-          </section>
+          <ScrollSync>
+            <section className="lyrics__group">
+              <ScrollSyncPane>
+                <textarea
+                  name="lyrics"
+                  id=""
+                  cols="30"
+                  rows="10"
+                  className="lyrics__input"
+                  onChange={handleLyricsInput}
+                />
+              </ScrollSyncPane>
+              <ScrollSyncPane>
+                <LyricsOutput
+                  lyrics={this.state.lyrics.result}
+                  colorsDB={colors}
+                />
+              </ScrollSyncPane>
+            </section>
+          </ScrollSync>
         </main>
       </RequirementWrapper>
     );
@@ -147,6 +72,9 @@ class Lyrics extends Component {
 }
 
 Lyrics.propTypes = {
+  admin: PropTypes.object.isRequired,
+  app: PropTypes.object.isRequired,
+  artists: PropTypes.object.isRequired,
   loadColors: PropTypes.func.isRequired,
 };
 
