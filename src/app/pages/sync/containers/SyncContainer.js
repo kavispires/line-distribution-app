5b344ac75345c4b48c024bec39fe533d3024c5f2
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-03-03 00:07:57 -0800

    Removed unused reducer in Sync pages

--- src/app/pages/sync/containers/SyncContainer.js
+++ src/app/pages/sync/containers/SyncContainer.js
@@ -5,21 +5,18 @@ import Sync from '../components/Sync';
 
 import { adminOperations } from '../../../../reducers/admin';
 import { artistsOperations } from '../../../../reducers/artists';
-import { authOperations } from '../../../../reducers/auth';
 import { syncOperations } from '../../../../reducers/sync';
 
 const mapStateToProps = state => ({
   admin: state.admin,
   app: state.app,
   artists: state.artists,
-  auth: state.auth,
   sync: state.sync,
 });
 
 const mapDispatchToProps = {
   ...adminOperations,
   ...artistsOperations,
-  ...authOperations,
   ...syncOperations,
 };
 
