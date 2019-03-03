af25cc1ab3aefb7fc5423ceb6003a2f29e47659a
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-03-03 00:22:35 -0800

    Added test to ensure reducer operations are not being overwritten

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,9 +1,9 @@
-import { appTypes } from './app';
-import { adminTypes } from './admin';
-import { artistsTypes } from './artists';
-import { authTypes } from './auth';
-import { syncTypes } from './sync';
-import { tempTypes } from './temp';
+import { appOperations, appTypes } from './app';
+import { adminOperations, adminTypes } from './admin';
+import { artistsOperations, artistsTypes } from './artists';
+import { authOperations, authTypes } from './auth';
+import { syncOperations, syncTypes } from './sync';
+import { tempOperations, tempTypes } from './temp';
 
 describe('Reducers', () => {
   it('it has no overlapping types', () => {
@@ -23,6 +23,32 @@ describe('Reducers', () => {
       Object.keys(typeGroup).forEach(type => {
         if (typeDict[type] === undefined) {
           typeDict[type] = true;
+        } else {
+          hasOverlapping = true;
+        }
+      })
+    );
+
+    expect(hasOverlapping).toBeFalsy();
+  });
+
+  it('it has no overlapping operatons', () => {
+    let hasOverlapping = false;
+    const operationsDict = {};
+
+    const allOperations = [
+      appOperations,
+      adminOperations,
+      artistsOperations,
+      authOperations,
+      syncOperations,
+      tempOperations,
+    ];
+
+    allOperations.forEach(operationGroup =>
+      Object.keys(operationGroup).forEach(operation => {
+        if (operationsDict[operation] === undefined) {
+          operationsDict[operation] = true;
         } else {
           hasOverlapping = true;
         }
