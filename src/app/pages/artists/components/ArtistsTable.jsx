fd1d28382dbe5ae681d4372dfe37c7d890ed41dd
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2019-06-06 19:33:37 -0700

    Added display count to artist table

--- src/app/pages/artists/components/ArtistsTable.jsx
+++ src/app/pages/artists/components/ArtistsTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 
@@ -11,19 +11,34 @@ const ArtistsTable = ({
   pending,
   rowAction,
   searchQuery,
+  showFavoriteArtistsOnly,
   user,
 }) => {
-  // Filter table rows based on searchQuery
+  // Filter artists
   let filteredArtists = artists;
-  if (searchQuery && filteredArtists[0]) {
-    filteredArtists = _.filter(artists, o => o.query.includes(searchQuery));
+  if ((searchQuery && filteredArtists[0]) || showFavoriteArtistsOnly) {
+    filteredArtists = _.filter(artists, o => {
+      let include = false;
+      // If displaying only favorite artists
+      if (showFavoriteArtistsOnly) {
+        include = user.favoriteArtists[o.id] !== undefined;
+      }
+      // If displaying only matching queries
+      if (searchQuery && filteredArtists[0]) {
+        include = o.query.includes(searchQuery);
+      }
+
+      return include;
+    });
   }
+
   // Message to be display when table has no rows
   const emptyTableMessage =
     searchQuery.length > 0
       ? 'No artists available within your search'
       : 'No artists available';
 
+  // Row Fallback when loading or no results
   const rowFallback = () => {
     if (pending) {
       return (
@@ -41,45 +56,60 @@ const ArtistsTable = ({
     );
   };
 
+  const hasActiveFilters = searchQuery.length > 0 || showFavoriteArtistsOnly;
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th className="artists-table__favorite-col" />
-          <th>Name</th>
-          <th>Genre</th>
-          <th>Units</th>
-          <th>Members</th>
-        </tr>
-      </thead>
-      <tbody onClick={rowAction}>
-        {filteredArtists.length > 0
-          ? filteredArtists.map(entry => (
-              <tr key={`all-artists-${entry.id}`} id={`a-${entry.id}`}>
-                <td
-                  className="artists-cell-favorite"
-                  onClick={() => favoriteAction(entry.id)}
-                >
-                  <FavoriteIcon
-                    action={() => {}}
-                    id={entry.id}
-                    size="12"
-                    state={
-                      user.favoriteArtists && user.favoriteArtists[entry.id]
-                    }
-                  />
-                </td>
-                <td>{entry.name}</td>
-                <td>{entry.genre}</td>
-                <td>{entry.units ? entry.units.length : 0}</td>
-                <td>
-                  {entry.memberList.join(', ')} ({entry.memberList.length})
-                </td>
-              </tr>
-            ))
-          : rowFallback()}
-      </tbody>
-    </table>
+    <Fragment>
+      {pending ? (
+        <p className="italic">Loading...</p>
+      ) : (
+        <p className="italic">
+          Displaying {filteredArtists.length} artists{hasActiveFilters
+            ? ' within search'
+            : ''}
+          ...
+        </p>
+      )}
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="artists-table__favorite-col" />
+            <th>Name</th>
+            <th>Genre</th>
+            <th>Units</th>
+            <th>Members</th>
+          </tr>
+        </thead>
+        <tbody onClick={rowAction}>
+          {filteredArtists.length > 0
+            ? filteredArtists.map(entry => (
+                <tr key={`all-artists-${entry.id}`} id={`a-${entry.id}`}>
+                  <td
+                    className="artists-cell-favorite"
+                    onClick={() => favoriteAction(entry.id)}
+                  >
+                    <FavoriteIcon
+                      action={() => {}}
+                      id={entry.id}
+                      size="12"
+                      state={
+                        user.favoriteArtists && user.favoriteArtists[entry.id]
+                      }
+                    />
+                  </td>
+                  <td>{entry.name}</td>
+                  <td>{entry.genre}</td>
+                  <td>{entry.units ? entry.units.length : 0}</td>
+                  <td>
+                    {entry.memberList.join(', ')} ({entry.memberList.length})
+                  </td>
+                </tr>
+              ))
+            : rowFallback()}
+        </tbody>
+      </table>
+    </Fragment>
   );
 };
 
@@ -89,12 +119,14 @@ ArtistsTable.propTypes = {
   pending: PropTypes.bool,
   rowAction: PropTypes.func.isRequired,
   searchQuery: PropTypes.string,
+  showFavoriteArtistsOnly: PropTypes.bool,
   user: PropTypes.object.isRequired,
 };
 
 ArtistsTable.defaultProps = {
   pending: false,
   searchQuery: '',
+  showFavoriteArtistsOnly: false,
 };
 
 export default ArtistsTable;
